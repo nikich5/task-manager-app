6610875645816889535b38793467e7bbb575fa28
nikich5 <enik3.ru@gmail.com>
2024-08-19 15:19:45 +0500

    typescript

--- webpack.common.config.js
+++ webpack.common.config.js
@@ -3,7 +3,19 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: {
-        app: './src/index.js',
+        app: './src/index.ts',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', 'ts', '.js'],
     },
     plugins: [
         new HtmlWebpackPlugin({
