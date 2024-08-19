2a8077d0351cb980db011acc1ff91910e3dcf0b3
nikich5 <enik3.ru@gmail.com>
2024-08-19 15:52:34 +0500

    fix

--- webpack.common.config.js
+++ webpack.common.config.js
@@ -27,7 +27,7 @@ module.exports = {
     },
     plugins: [
         new HtmlWebpackPlugin({
-            title: 'Production',
+            title: 'Task Manager',
         }),
     ],
     output: {
